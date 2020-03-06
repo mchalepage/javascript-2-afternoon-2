59156de4b173e4059f9894b53a6b86851ee1eee5
mchalepage <me@mchale.page>
2020-03-05 18:56:21 -0700

    finished all problems

--- practice.js
+++ practice.js
@@ -123,7 +123,17 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
   and the second item in the array being the odds array (all the odd values from numbersArray).
 */
 
-//Code Here
+let divider = (numbersArray) => {
+  let hurrayArray = [[],[]]
+  for (let i = 0; i < numbersArray.length; i++){
+    if(numbersArray[i] % 2 === 0){
+      hurrayArray[0].push(numbersArray[i])
+    } else {
+      hurrayArray[1].push(numbersArray[i])
+    }
+  }
+  return hurrayArray
+}
 
 
 
@@ -135,17 +145,28 @@ var getRandomArbitrary = function() {
 };
 // Do not edit the code above.
 
+//var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 /* 
-  var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
   Above you're given a function (getRandomArbitrary) that will return a random number between 0 and 30.
   There is also a commented out array full of numbers to help you visualize what your function will be receiving.
+
   Write a function named finder that will take in an array as an argument.
   In the function create a variable called randomNumber and set it to the invocation of getRandomArbitrary.
   Loop through the array to see if randomNumber is in the array. 
   If it is, return true, if it's not, return false
 */
 
-//Code Here
+let finder = (arr) => {
+  let randomNumber = getRandomArbitrary()
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] === randomNumber){
+      return true;
+    }
+  }
+  return false;
+}
+
+
 
 
 
@@ -174,7 +195,27 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
   addItem() --> [];
 */
 
-//Code Here
+let removeItem = (myGroceryList, item) => {
+  if(myGroceryList && item){
+    for (i = 0; i < myGroceryList.length; i++){
+      if (myGroceryList[i] === item){
+        myGroceryList.splice(i, 1)
+      }
+    }
+    return myGroceryList
+  } else {
+    return []
+  }
+}
+
+let addItem = (myGroceryList, item) => {
+  if (myGroceryList && item){
+    myGroceryList.push(item)
+    return myGroceryList
+  } else {
+    return []
+  }
+}
 
 
 
@@ -184,7 +225,13 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
   Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
 */
 
-//Code Here
+let maker = () => {
+  let numArray = []
+  for (let i = 0; i < 215; i++){
+    numArray[i] = i + 1
+  }
+  return numArray
+}
 
 
 
@@ -200,7 +247,15 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
   Your output should look like this -> [15, 19, 26, 29, 35, 44, 58]
 */
   
-//Code Here
+let addTen = (numbers) => {
+  let newNumbers = []
+  for (let i = 0; i < numbers.length; i++){
+    newNumbers.push(Number(numbers[i]))
+    newNumbers[i] += 10
+  }
+  return newNumbers
+}
+addTen(numbers)
 
 
 
@@ -225,7 +280,13 @@ for(var i = 0; i < num2; i++){
   Return the longer of the two arrays.
 */
 
-//Code Here
+let longer = (arr1, arr2) => {
+  if (arr1.length > arr2.length){
+    return arr1
+  } else {
+    return arr2
+  }
+}
 
 
 
@@ -237,8 +298,16 @@ for(var i = 0; i < num2; i++){
   Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
 */
 
-//Code Here
-
+let both = (arr1, arr2) => {
+  let matchingNumbers = []
+  for (let i = 0; i < arr1.length; i++){
+    for (let x = 0; x < arr2.length; x++){
+      if(arr1[i] = arr2[x]){
+        matchingNumbers.push(arr1[i])
+      }
+    }
+  } return matchingNumbers
+}
 
 
 ////////// PROBLEM 12 //////////
@@ -278,7 +347,7 @@ var colt = {
   After that, console.log the length of the Array and make sure that it's equal to 4. 
 */
 
-//Code Here
+devMountainEmployees = [joe, cahlan, ryan, colt]
 
 
 
@@ -287,7 +356,7 @@ var colt = {
   Loop through your devMountainEmployees until you find cahlan, then remove him from the array.
 */
 
-//Code Here
+devMountainEmployees.splice(1, 1)
 
 
 
@@ -299,7 +368,7 @@ var colt = {
   Create an empty array called users.
 */
 
-//Code Here
+let users = []
 
 
 
@@ -320,7 +389,21 @@ var user1 = {
 };
 // Do not edit the code above.
 
-//Code Here
+users = [
+  user1,
+  user2 = {
+    name: "McHale",
+    email: "email@email.com",
+    password: "secret",
+    username: "secret"
+  },
+  user3 = {
+    name: "McHale",
+    email: "email@email.com",
+    password: "secret",
+    username: "secret"
+  },
+]
 
 
 
@@ -334,7 +417,7 @@ var user1 = {
   Once you find the array index he's located in, delete him from the array.
 */
 
-//Code Here
+users.splice(0,1)
 
 
 
